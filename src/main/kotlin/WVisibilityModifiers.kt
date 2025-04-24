e7bab4564d97b98a7f89bcde1dadefcd7e9e59f2
KishorRamani <kishorkeepsmiling@gmail.com>
2025-04-24 08:41:28 +0530

    2:49:29 - Visibility modifiers 2:53:06 - Generics

--- src/main/kotlin/WVisibilityModifiers.kt
+++ src/main/kotlin/WVisibilityModifiers.kt
@@ -1,5 +1,46 @@
 package com.kishorramani
 
-fun main() {
+import kotlin.math.sqrt
 
+fun main() {
+    val rect = Rectangle9(
+        width = 15f,
+        height = 10f
+    )
+
+    rect.diagonal       //It's accessible because it's public
+    //rect.totalHeight        //it's not public
+
+    rect.area
+
+    //rect.counter        //This is not accessible outside the child class because it's protected
+    rect.counter1       //It's public
+}
+
+data class Rectangle9(
+    val width: Float,
+    val height: Float
+) : Shape9, Shape10() {
+    val diagonal = sqrt(width * width + height * height)
+
+    private val totalHeight = height * 2
+
+    override val area: Float get() = width * height
+
+    override val circumference = 2 * width + 2 * height
+
+    override val counter: Int = 10
+    override val counter1: Int get() = 15
+    override val counter2: Int get() = 25
+}
+
+sealed interface Shape9 {
+    val area: Float
+    val circumference: Float
+}
+
+abstract class Shape10 {
+    protected abstract val counter: Int     //This is accessible in the child class but's not outside the class
+    abstract val counter1: Int
+    internal abstract val counter2: Int     //Internal means it's accessible inside the module, not outside the module, it's useful when we can provide our module as a lib rary
 }
--- src/main/kotlin/XGenerics.kt
+++ src/main/kotlin/XGenerics.kt
@@ -1,5 +1,67 @@
 package com.kishorramani
 
-fun main() {
+import javax.sql.rowset.Predicate
 
+fun main() {
+    val myFavoriteNumber = intArrayOf(1, 2, 3, 4, 5, 6)
+    val helloWorld = "Hello world"
+
+    val evenNumber = myFavoriteNumber.filter { it % 2 == 0 }        //myFavoriteNumber is an array of int
+    val lettersOnly = helloWorld.filter { it.isLetter() }           //helloWorld is an array of characters
+    //here is the filter function is generic
+
+    val list = listOf(1, 2, 2, 3).filter { it % 2 == 0 }      //here, filter is generic
+    /*public inline fun <T> Iterable<T>.filter(
+        predicate: (T) -> Boolean
+    ): List<T>*/
+
+    println(evenNumber)
+    println(lettersOnly)
+
+    val stringList = listOf(
+        "Hello",
+        "World",
+        "How are you"
+    )
+    val intList = listOf(1, 2, 3, 4, 5)
+
+    val filteredString = stringList.myFilter { currentString ->
+        currentString.length < 10
+    }
+    val filteredInt = intList.filter { it < 3 }
+
+
+    println(filteredString)
+    println(filteredInt)
+
+    val result = makeNetworkCall()
+    println(result)
+
+    val filteredIntMapToString = intList.map { it.toString() }     //T - Type of class we pass(Int), R - Type we want to map the data
+    val filteredIntMapToDouble = intList.map { it.toDouble() }     //T - Type of class we pass(Int), R - Type we want to map the data
+    /*public inline fun <T, R> Iterable<T>.map(
+        transform: (T) -> R
+    ): List<R>*/
+    println(filteredIntMapToString)     //output array is a string array
+    println(filteredIntMapToDouble)     //output array is a string array
+}
+
+fun <T> List<T>.myFilter(predicate: (T) -> Boolean): List<T> {
+    val result = mutableListOf<T>()
+    for (element in this) {
+        if (predicate(element)) {
+            result.add(element)
+        }
+    }
+    return result.toList()
+}
+
+sealed interface Result<out D, out E> {     //out - use for return type is not decided yet, it will be created based on result return type
+    data class Success<D>(val data: D): Result<D, Nothing>      //Nothing is a special class in kotlin
+    data class Failure<E>(val error: E): Result<Nothing, E>
+}
+
+fun makeNetworkCall(): Result<Int, String> {
+    //return Result.Success()
+    return Result.Failure("Something went wrong", )
 }
